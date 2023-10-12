8e597dbab88d440434e11009d57eef804e80096b
hieundAmela <hieu.nguyenduy@amela.vn>
2023-10-12 17:33:45 +0700

    feature: ui auth

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ConfigProvider as AntdConfigProvider } from "antd";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RecoilRoot } from "recoil";
 import {
+  Navigate,
   Route,
   RouterProvider,
   createBrowserRouter,
@@ -11,6 +12,8 @@ import { Suspense } from "react";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Home from "./pages/home";
 import ErrorPage from "./pages/error";
+import AuthPage from "./pages/auth";
+import Cookies from "js-cookie";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,18 +25,36 @@ const queryClient = new QueryClient({
   },
 });
 
+const isAuth = Cookies.get("token");
+
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<ErrorPage />}/>
+      <Route
+        path="/auth"
+        element={!isAuth ? <AuthPage /> : <Navigate to="/" />}
+      />
+      <Route path="*" element={<ErrorPage />} />
     </Route>
   )
 );
 
+const theme = {
+  token: {
+    colorPrimary: "#895a42",
+  },
+  components: {
+    Form: {
+      labelFontSize: "1rem",
+      labelColor: "#895a42",
+    },
+  },
+};
+
 function App() {
   return (
-    <AntdConfigProvider>
+    <AntdConfigProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <RecoilRoot>
           <RouterProvider router={router} />
