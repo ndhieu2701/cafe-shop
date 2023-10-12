eabb171c212371ab9f4bce7978f145096b01be27
hieundAmela <hieu.nguyenduy@amela.vn>
2023-10-12 11:28:23 +0700

    config: config axios

---
+++ src/api/axios.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import configs from "../config";
+import Cookies from "js-cookie";
+
+const axiosInstance = axios.create({
+  timeout: 3 * 60 * 1000,
+  baseURL: configs.API_DOMAIN,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = `Bearer ${Cookies.get("token")}`;
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response.status !== 401) {
+      return Promise.reject(error);
+    }
+  }
+);
+
+export const sendGet = (url, params) =>
+  axiosInstance.get(url, { params }).then((res) => res.data);
+
+export const sendPost = (url, params, queryParams) =>
+  axiosInstance
+    .post(url, params, { params: queryParams })
+    .then((res) => res.data);
+
+export const sendPut = (url, params) =>
+  axiosInstance.put(url, params).then((res) => res.data);
+
+export const sendPatch = (url, params) =>
+  axiosInstance.patch(url, params).then((res) => res.data);
+
+export const sendDelete = (url, params) =>
+  axiosInstance.delete(url, { params }).then((res) => res.data);
---
+++ src/config/index.js
@@ -0,0 +1,6 @@
+const configs = {
+    BASE_URL: import.meta.env.VITE_BASE_URL,
+    API_DOMAIN: import.meta.env.VITE_API_URL
+}
+
+export default configs
